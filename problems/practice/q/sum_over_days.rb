d5e81c82f4cf05fa074a01117fdfcbb5df996eee
James Edward Gray II <james@graysoftinc.com>
2010-06-05 11:38:56 -0500

    Adding a coefficients helper.

--- problems/practice/q/sum_over_days.rb
+++ problems/practice/q/sum_over_days.rb
@@ -1,3 +1,8 @@
+def coefficients(size)
+  counts = (0...(size / 2.0).ceil).map { |i| (i + 1) * size - i ** 2 - i }
+  size.even? ? counts + counts.reverse : counts + counts[0..-2].reverse
+end
+
 tests = ARGF.gets.to_i
 tests.times do
   ARGF.gets  # throw away blank line

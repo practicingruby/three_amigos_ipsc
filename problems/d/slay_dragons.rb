93ddb7ee843f18e8b9db6c8dc65689b4f42164ba
James Edward Gray II <james@graysoftinc.com>
2010-06-06 07:52:43 -0500

    Adding a contraint.

--- problems/d/slay_dragons.rb
+++ problems/d/slay_dragons.rb
@@ -2,9 +2,15 @@ def solve(n, c1, g1, c2, g2)
   winner   = "DRAGON"
   s1_delta = c1 - g1
   s2_delta = c2 - g2
-  0.upto(n) do |a|
-    last = nil
-    0.upto(n) do |b|
+  if s1_delta <= 0 and s2_delta <= 0
+    case n
+    when c1, c2 then         return "PRINCE"
+    when c1 + 1, c2 + 1 then return "DRAW"
+    else                     return "DRAGON"
+    end
+  end
+  0.upto(10_000) do |a|
+    0.upto(10_000) do |b|
       eqs = [ a * s1_delta + b * s2_delta - n + c1,
               a * s1_delta + b * s2_delta - n + c2 ]
       if eqs.any? { |eq| eq == 0 }
@@ -12,9 +18,6 @@ def solve(n, c1, g1, c2, g2)
       elsif eqs.any? { |eq| eq == 1 }
         winner = "DRAW"
       end
-      break if last and last.first.abs < eqs.first.abs and
-                        last.last.abs  < eqs.last.abs
-      last = eqs
     end
   end
   winner
@@ -27,4 +30,5 @@ tests.times do
   n, c1, g1, c2, g2 = ARGF.gets.split.map { |n| n.to_i }
   
   puts solve(n, c1, g1, c2, g2)
+  $stdout.flush
 end
